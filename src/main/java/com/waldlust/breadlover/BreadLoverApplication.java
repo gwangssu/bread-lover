d6898109e2aaaab7bd775ba34bf49622e4b46a11
uy <uy@waldlust.co.kr>
2024-03-04 16:58:04 +0900

    프로젝트 세팅

---
+++ src/main/java/com/waldlust/breadlover/BreadLoverApplication.java
@@ -0,0 +1,13 @@
+package com.waldlust.breadlover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadLoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreadLoverApplication.class, args);
+    }
+
+}
